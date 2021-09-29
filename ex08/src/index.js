51024aaddc619b19f594b3881cc484e4d5195cc0
okiceokano <57456536+okiceokano@users.noreply.github.com>
2021-09-29 09:37:27 +0200

    +day17

---
+++ ex08/src/index.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+
+var users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 24,
+        gender: "male"
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 5,
+        gender: "female"
+    },
+    {
+        firstName: "Jim",
+        lastName: "Carrey",
+        age: 54,
+        gender: "male"
+    },
+    {
+        firstName: "Kate",
+        lastName: "Winslet",
+        age: 40,
+        gender: "female"
+    }
+];
+
+function getUsers() {
+    var output = "";
+    for (i = 0; i < users.length; i++) {
+        users[i] = `${users[i].firstName}` + " " + `${users[i].lastName}` + " is " + `${users[i].age}` + ", " + `${users[i].gender}\n`;
+        output += users[i];
+
+    }
+    return output;
+}
+
+function findUser(lastName, gender) {
+    try {
+        var user = _.find(users, { lastName: lastName, gender: gender });
+        var iFindUser = `${user.firstName}` + " " + `${user.lastName}` + " is " + `${user.age}` + ", " + `${user.gender}`;
+        return iFindUser;
+    } catch (error) {
+        return "Cannot read property 'firstName' of undefined";
+    }
+}
+
+getUsers();
+console.log(findUser('Doe', 'male'));
+
+module.exports = findUser;
